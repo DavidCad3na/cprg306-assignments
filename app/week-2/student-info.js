1c968d6c6b2bd05527f145d833b6fd9b833710d5
David Cadena <Cadenadavid25@gmail.com>
2025-09-15 14:59:33 -0600

    Week 2 Lab

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+export default function StudentInfo() {
+  return (
+    <main>
+    <h1>Name: David Cadena</h1>
+      <Link href="https://github.com/DavidCad3na/cprg306-assignments" target="_blank">
+        GitHub: DavidCad3na/cprg306-assignments
+      </Link>
+      </main>
+  );
+}
